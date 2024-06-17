645465e9498f80374c5717be6688446cbbfb2471
Alper <alper@alper.com>
2024-06-17 03:41:12 +0300

    merge backend frontend

---
+++ app-backend/app/Http/Controllers/CatController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cat;
+
+class CatController extends Controller
+{
+    public function index()
+    {
+        $cats = Cat::all();
+        return response()->json($cats);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Request $request)
+    {
+        $cat = Cat::create($request->all());
+        return response()->json($cat);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $cat = Cat::create($request->all());
+        return response()->json(['message' => 'Cat created!', 'cat' => $cat]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Cat $cat)
+    {
+        return response()->json($cat);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Cat $cat)
+    {
+        $cat->update($request->all());
+        return response()->json(['message' => 'Cat updated!', 'cat' => $cat]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Cat $cat)
+    {
+        $cat->delete();
+        return response()->json(['message' => 'Cat deleted :(']);
+=======
+    /**
+     * Display a listing of the cats.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cats = Cat::all();
+        return view('cats.index', compact('cats'));
+    }
+
+    /**
+     * Show the form for choosing a specific cat.
+     *
+     * @param  \App\Models\Cat  $cat
+     * @return \Illuminate\Http\Response
+     */
+    public function choose(Cat $cat)
+    {
+        return view('cats.choose', compact('cat'));
+    }
+}
